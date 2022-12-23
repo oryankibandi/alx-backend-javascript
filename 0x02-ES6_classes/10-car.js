b9981c67c11e8bbbfec087da2090b01d22b3d633
oryankibandi <shreckoryan@gmail.com>
2022-12-23 23:28:05 +0300

    EVCar

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const extendingClass = this.constructor.toString().split(" ")[1];
+    if (extendingClass == "EVCar") {
+      return Object.create(Car.prototype);
+    } else {
+      return Object.create(this, Object.getOwnPropertySymbols(this));
+    }
+  }
+}
+
+module.exports = Car;
---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+import Car from "./10-car";
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._color = color;
+    this._motor = motor;
+    this._range = range;
+  }
+}
+
+module.exports = EVCar;
